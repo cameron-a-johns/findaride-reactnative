b99f0b5af7a4ff8b6446679cca9a73f430f1760d
CJ <cameron.a.johns@gmail.com>
2020-01-04 22:20:16 +1100

    Add missed files

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-native';
+import { Switch, Route, NativeRouter } from 'react-router-native';
 import App from '../../App';
-import {LoginView} from '../views/login';
+import { LoginView } from '../views/login';
 
 export const routes = () => {
   return (
-    <Switch>
-      <Route path="/" component={LoginView} />
-      <Route path="/app" component={App} />
-    </Switch>
+    <NativeRouter>
+      <Switch>
+        <Route path="/" component={LoginView} />
+        <Route path="/app" component={App} />
+      </Switch>
+    </NativeRouter>
   );
 };
--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import {LoginButton} from 'react-native-fbsdk';
+import { View, Text } from 'react-native';
+import { LoginButton } from 'react-native-fbsdk';
 
 export const LoginView: React.FC = () => {
   return (
     <>
+      {console.log('test')}
       <View>
         <Text>Login</Text>
         <LoginButton
           publishPermissions={['public_profile']}
           onLoginFinished={(error, result) => {
+            console.log('login finished');
             if (error) {
               alert('Login failed with error: ' + error.message);
             } else if (result.isCancelled) {
               alert('Login was cancelled');
             } else {
-              alert(
-                'Login was successful with permissions: ' +
-                  result.grantedPermissions,
-              );
+              console.log(result);
+              alert('Login was successful with permissions: ' + result.grantedPermissions);
             }
           }}
           onLogoutFinished={() => alert('User logged out')}
